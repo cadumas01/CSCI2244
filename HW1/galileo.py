a2e095c19e855aa253c91ac65aa72278188891dc
Cole <cadumas01@gmail.com>
2022-01-26 17:35:55 -0500

    Working but not sure about # of tests necessary

--- HW1/galileo.py
+++ HW1/galileo.py
@@ -1,7 +1,4 @@
-import numpy as np
-import math
-import matplotlib.mlab as mlab
-import matplotlib.pyplot as plt
+import numpy
 
 
 def galileo(d, a, b):
@@ -18,8 +15,9 @@ def galileo(d, a, b):
 
 # Rounds the sample relative_frequency of x to its nearest 'integer' step
 def prob(x, d):
-    t = 6**(2*d)
-    n = math.ceil(t / d)  # not sure how many rolls to do.
+    # t = 6**(2*d)
+    # n = math.ceil(t / d)  # not sure how many rolls to do.
+    n = 2315 * ( 6 ** d)
     arr = sum_dice_roll(n, d)
     prob_step = 1 / (6 ** d)
 
@@ -31,13 +29,13 @@ def prob(x, d):
 
 # Returns the frequency of x occurring in arr
 def freq(x, arr):
-    num_x = np.equal(arr, x)
-    return np.count_nonzero(num_x)
+    num_x = numpy.equal(arr, x)
+    return numpy.count_nonzero(num_x)
 
 
 # Creates a d by n array of random integers [1,6]
 def dice_roll(n, d):
-    return np.random.randint(1, 7, size=(n, d))
+    return numpy.random.randint(1, 7, size=(n, d))
 
 
 # Creates a 1 by n array of the sum of d dice rolls in each collum; (vertically sums d dice_roll arrays)
@@ -45,21 +43,10 @@ def sum_dice_roll(n, d):
     return dice_roll(n, d).sum(axis=1)
 
 
-def hist(n, d):
-    arr = sum_dice_roll(n,d)
-    bins = 6*d - d + 1
-    plt.hist(arr, density=True, bins=bins)
-    plt.ylabel("Frequency")
-    plt.xlabel("Data")
-    plt.show()
-    print(bins)
-
-
 if __name__ == "__main__":
-    # hist(5000000,2)
     zeros = 0
     not_zeros = 0
-    for i in range(1000):
+    for i in range(100):
         if (galileo(3,10,11)) == 0:
             zeros = zeros + 1
         else:
