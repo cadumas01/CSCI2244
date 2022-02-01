f04894f187524a65d1c360b79c84b0a713ac5ed6
Cole <cadumas01@gmail.com>
2022-02-01 12:11:50 -0500

    done

--- HW1/galileo.py
+++ HW1/galileo.py
@@ -15,13 +15,11 @@ def galileo(d, a, b):
 
 # Rounds the sample relative_frequency of x to its nearest 'integer' step
 def prob(x, d):
-    # t = 6**(2*d)
-    # n = math.ceil(t / d)  # not sure how many rolls to do.
-    n = 2315 * ( 6 ** d)
-    arr = sum_dice_roll(n, d)
+    t = (2 * (6 ** d)) ** 2
+    arr = sum_dice_roll(t, d)
     prob_step = 1 / (6 ** d)
 
-    x_rel_freq = freq(x, arr)/n
+    x_rel_freq = freq(x, arr)/t
 
     # finds the nearest relative frequency in prob_step units
     return round(x_rel_freq/prob_step) * prob_step
@@ -34,22 +32,11 @@ def freq(x, arr):
 
 
 # Creates a d by n array of random integers [1,6]
-def dice_roll(n, d):
-    return numpy.random.randint(1, 7, size=(n, d))
+def dice_roll(t, d):
+    return numpy.random.randint(1, 7, size=(t, d))
 
 
 # Creates a 1 by n array of the sum of d dice rolls in each collum; (vertically sums d dice_roll arrays)
-def sum_dice_roll(n, d):
-    return dice_roll(n, d).sum(axis=1)
+def sum_dice_roll(t, d):
+    return dice_roll(t, d).sum(axis=1)
 
-
-if __name__ == "__main__":
-    zeros = 0
-    not_zeros = 0
-    for i in range(100):
-        if (galileo(3,10,11)) == 0:
-            zeros = zeros + 1
-        else:
-            not_zeros = not_zeros + 1
-
-    print("Zeros =", zeros, "\n", "Not Zeros =", not_zeros)
